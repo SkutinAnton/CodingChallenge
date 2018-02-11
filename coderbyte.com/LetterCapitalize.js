0c6107cdd0fbde6a4c6183c56f3bc324d48959d5
SkutinAnton <Skutin_anton@mail.ru>
2018-02-11 20:56:32 +0300

    add new solved task

---
+++ coderbyte.com/LetterCapitalize.js
@@ -0,0 +1,23 @@
+/*
+https://coderbyte.com
+
+Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word.
+Words will be separated by only one space.
+*/
+
+
+function LetterCapitalize(str) {
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (i === 0 && str[i] !== ' ') {
+            newStr += str[i].toUpperCase();
+        } else if (str[i] !== ' ' && str[i - 1] === ' ') {
+            newStr += str[i].toUpperCase();
+        } else {
+            newStr += str[i];
+        }
+    }
+
+    return newStr;
+}

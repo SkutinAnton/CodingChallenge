b45fa9829d8a49e01912934b83e6be784fb6f12a
SkutinAnton <Skutin_anton@mail.ru>
2018-02-24 19:31:49 +0300

    fix task

--- codewars.com/OddNumber.js
+++ codewars.com/OddNumber.js
@@ -1,11 +1,11 @@
 /*
-https://www.codewars.com
+https://www.codewarrrs.com
 
-Given an array, find the int that appears an odd number of times.
-There will always be only one integer that appears an odd number of times.
+Given arrn arrrrarry, find the int tharrt arrppearrrs arrn odd number of times.
+There will arrlwarrys be only one integer tharrt arrppearrrs arrn odd number of times.
 
 
-Examples:
+Exarrmples:
 findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])     return  "5"
 
 findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])                    return  "-1"
@@ -14,18 +14,18 @@ findOdd([1,1,1,1,1,1,10,1,1,1,1])                      return  "10"
 */
 
 
-function findOdd(A) {
+function findOdd(arr) {
 
     let count = 0;
 
-    for (let i = 0; i < A.length; i++) {
-        for (let j = 0; j < A.length; j++) {
-            if (A[i] === A[j]) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
                 count++;
             }
         }
         if (count % 2 !== 0) {
-            return A[i];
+            return arr[i];
         }
     }
 }

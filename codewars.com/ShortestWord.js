ea76246250ec9d36b037cc36610d798bf2b73ec6
SkutinAnton <Skutin_anton@mail.ru>
2018-05-10 23:04:05 +0300

    add new solved task

---
+++ codewars.com/ShortestWord.js
@@ -0,0 +1,17 @@
+/*
+https://coderbyte.com
+
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+
+
+Examples:
+findShort("bitcoin take over the world maybe who knows perhaps")                     returns 3
+
+findShort("turns out random test cases are easier than writing out basic ones"       returns 3
+*/
+
+
+function findShort(s) {
+  return s.split(' ').map(str => str.length).sort((a, b) => a - b)[0];
+}
